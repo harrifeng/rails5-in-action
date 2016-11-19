161fd3089a59d3634f16fa81e0b415556da2aaa0
harri feng <harri.feng@gmail.com>
2016-11-19 07:28:36 +0000

    need to make the validte jpg work

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
+  validates :price, numericality: { greater_then_or_equal_to: 0.01}
+  validates :title, uniqueness: true
 end
